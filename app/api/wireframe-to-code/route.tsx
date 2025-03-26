892011adae55526b41c79caa1c6f70c76117ccc0
Sujeet Brahmankar <sujeetbrahmankar@Sujeet-Brahmankars-MacBook-Pro.local>
2025-03-26 19:45:21 +0530

    First version of code is ready

---
+++ app/api/wireframe-to-code/route.tsx
@@ -0,0 +1,71 @@
+import { db } from "@/configs/db";
+import { usersTable, WireframeToCodeTable } from "@/configs/schema";
+import { desc, eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req: NextRequest) {
+    const { description, imageUrl, model, uid, email } = await req.json();
+    console.log(uid)
+
+    const creditResult = await db.select().from(usersTable)
+        .where(eq(usersTable.email, email));
+
+    if (creditResult[0]?.credits && creditResult[0]?.credits > 0) {
+
+        const result = await db.insert(WireframeToCodeTable).values({
+            uid: uid,
+            description: description,
+            imageUrl: imageUrl,
+            model: model,
+            createdBy: email
+        }).returning({ id: WireframeToCodeTable.id })
+
+        // Update user credits
+        const data = await db.update(usersTable).set({
+            credits: creditResult[0]?.credits - 1
+        }).where(eq(usersTable.email, email));
+
+        return NextResponse.json(result);
+    }
+    else {
+        return NextResponse.json({ 'error': 'Not enough credits' })
+    }
+}
+
+
+export async function GET(req: NextResponse) {
+    const reqURL = req.url;
+    const { searchParams } = new URL(reqURL);
+    const uid = searchParams?.get('uid');
+    const email = searchParams?.get('email');
+    if (uid) {
+        const result = await db.select()
+            .from(WireframeToCodeTable)
+            .where(eq(WireframeToCodeTable.uid, uid));
+        return NextResponse.json(result[0])
+    }
+    else if (email) {
+        const result = await db.select()
+            .from(WireframeToCodeTable)
+            .where(eq(WireframeToCodeTable.createdBy, email))
+            .orderBy(desc(WireframeToCodeTable.id))
+            ;
+        return NextResponse.json(result);
+    }
+
+    return NextResponse.json({ error: 'No Record Found' })
+}
+
+export async function PUT(req: NextRequest) {
+    const { uid, codeResp } = await req.json();
+
+    const result = await db.update(WireframeToCodeTable)
+        .set({
+            code: codeResp
+        }).where(eq(WireframeToCodeTable.uid, uid))
+        .returning({ uid: WireframeToCodeTable.uid })
+
+    return NextResponse.json(result);
+
+}
